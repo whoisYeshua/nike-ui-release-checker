b8043cb4c76c2c6fbc32e931e7691b63f19b6cb3
whoisYeshua <alien19987@gmail.com>
2025-07-19 10:31:43 +0300

    implement base select logic

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,7 +1,14 @@
+<script lang="ts">
+	import CountrySelect from './CountrySelect.svelte';
+</script>
+
 <main>
-	<section>
-		<h2>Upcoming releases</h2>
-		<div class="releases">
+	<div class="top-panel">
+		<CountrySelect />
+	</div>
+	<section class="releases">
+		<h2 class="releases__title">Upcoming releases</h2>
+		<div class="releases__grid">
 			<div style="width: 180px; height: 180px; background-color: red"></div>
 			<div style="width: 180px; height: 180px; background-color: red"></div>
 			<div style="width: 180px; height: 180px; background-color: red"></div>
@@ -34,30 +41,35 @@
 		}
 	}
 
-	section {
+	.top-panel {
+		display: flex;
+		justify-content: center;
+	}
+
+	.releases {
 		display: flex;
 		flex-direction: column;
 		gap: 1rem;
 	}
 
-	h2 {
+	.releases__title {
 		font-size: 1.5rem;
 		line-height: calc(1.5rem + 4px);
 		text-transform: uppercase;
 	}
 
-	.releases {
+	.releases__grid {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(min(360px, 100%), 1fr));
-		column-gap: 20px;
-		row-gap: 80px;
+		column-gap: 1.25rem;
+		row-gap: 5rem;
 
 		@media (width <= 1760px) {
-			row-gap: 60px;
+			row-gap: 3.75rem;
 		}
 
 		@media (width <= 640px) {
-			row-gap: 40px;
+			row-gap: 2.5rem;
 		}
 	}
 </style>
