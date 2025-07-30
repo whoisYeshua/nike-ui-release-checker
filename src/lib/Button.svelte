f2eb0e267256fc9d2786fd58d5349b30d80920b4
whoisYeshua <alien19987@gmail.com>
2025-07-30 16:19:20 +0300

    Enhance Button component: add ontouchstart event for improved touch support on Safari and remove redundant font-family declaration for cleaner CSS.

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -21,11 +21,13 @@
 	};
 </script>
 
+<!-- ontouchstart enable CSS pseudo-class `:active` work on safari  https://ru.stackoverflow.com/questions/1066115/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D1%87%D1%82%D0%BE%D0%B1%D1%8B-%D0%BD%D0%B0-ios-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D0%BB-active-%D1%83-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2 -->
 <button
 	class="button"
 	class:light={variant === 'light'}
 	onclick={handleClick}
 	style:padding={text ? '6px 16px' : '6px'}
+	ontouchstart={() => {}}
 	{...rest}
 >
 	{@render icon?.()}
@@ -60,7 +62,6 @@
 		font-weight: normal;
 		font-size: 16px;
 		line-height: 20px;
-		font-family: 'Helvetica Neue', sans-serif;
 
 		-webkit-user-select: none;
 		user-select: none;
